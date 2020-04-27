de94daa7e5c19edb594ae02bccccc9e1d9183f71
Sumit <sumit.thakur@gorapid.io>
2020-04-27 18:14:12 +0530

    Adding changes in create note screen storing notes in collection.
        * This addresses issue #2.
        * Storing notes in "notes" collection
          and displaying list on homeScreen

--- lib/create.dart
+++ lib/create.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:provider/provider.dart';
+import './model/user.dart' show CurrentUser;
 
 // Since we are handling user input, state is used
 class NoteCreate extends StatefulWidget {
@@ -17,11 +19,17 @@ class NoteCreate extends StatefulWidget {
 
 class NoteCreateState extends State<NoteCreate> {
   final collection = Firestore.instance.collection('tasks');
+  final notesCollection = Firestore.instance.collection('notes');
+
   // Controller that handles the TextField
   final TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    final user = Provider
+        .of<CurrentUser>(context)
+        ?.data;
+    final uid = user?.uid;
     return Scaffold(
       appBar: AppBar(title: Text('Create a task')),
       body: Center(
@@ -37,8 +45,8 @@ class NoteCreateState extends State<NoteCreate> {
         child: Icon(Icons.done),
         onPressed: () async {
           if (controller.text.isNotEmpty) {
-            await collection.add(
-              {'name': controller.text, 'completed': false},
+            await notesCollection.add(
+              {'title': controller.text, 'state': 0, 'uid': uid},
             );
           } else {
             //TODO: field validation
